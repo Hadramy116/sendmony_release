c1c91614d1d76278e2feed86347eacc31ad9b331
Hadramy Ahmed <hadramy@ebdih.com>
2020-01-25 20:46:52 +0000

    inisialize sendMoney app

---
+++ src/test/java/mr/send/money/sendmoney/SendMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.send.money.sendmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
